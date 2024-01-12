e72f39f5f7107031a92bfe89296b0105d803a2ef
LucasLrye <lucaslaroye25@gmail.com>
2024-01-12 18:31:05 +0100

    92%,super pointer, thread, macro

---
+++ exercises/clippy/target/debug/deps/clippy1-e4d061c1daac2044.d
@@ -0,0 +1,9 @@
+/home/laroye/Rust_dossier/rustlings/exercises/clippy/target/debug/deps/libclippy1-e4d061c1daac2044.rmeta: clippy1.rs Cargo.toml
+
+/home/laroye/Rust_dossier/rustlings/exercises/clippy/target/debug/deps/clippy1-e4d061c1daac2044.d: clippy1.rs Cargo.toml
+
+clippy1.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,13 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+
+use crate::List::{Cons, Nil};
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -35,11 +37,13 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    let list = Box::new(Nil);
+    *list
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    list
 }
 
 #[cfg(test)]
